609ddcaa51d2ff526d8abadfdc8a0b09c9a5cfa5
flecaros84 <fabi.lecaros@duocuc.cl>
2025-06-15 18:17:57 -0400

    Se renombra bicicleta-service a catalogo-service, se agrega Accesorios al modelo

---
+++ masterbikes/catalogo-service/src/main/java/masterbikes/catalogo_service/model/Accesorio.java
@@ -0,0 +1,22 @@
+package masterbikes.catalogo_service.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class Accesorio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String modelo;           // “Casco Bell Sanction”
+    private String categoria;        // “Casco”, “Luz”, “Bomba”
+    private String marca;
+    private String descripcion;
+    private String talla;
+    private String tipoUso;         // "Montaña", "Ruta", etc.
+}
